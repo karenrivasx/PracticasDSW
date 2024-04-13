6ff8b562ab7ce6d059eff772689b9239537f9c46
karenrivasx <rivas.karen@uabc.edu.mx>
2024-04-13 02:22:57 -0700

    alerta huracan

---
+++ practica24/practica24.js
@@ -0,0 +1,12 @@
+function avisoHuracan(hayUnHuracan) {
+    if (hayUnHuracan) {
+      alert("¡Hay un Huracán!");
+    } else {
+      alert("Está Soleado");
+    }
+  }
+  
+  // Ejemplo de uso
+  var hayHuracan = confirm("¿Hay un huracán?"); // Esto mostrará un cuadro de confirmación al usuario
+  avisoHuracan(hayHuracan);+
+
