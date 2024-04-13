0b736cc72abfb550dfd08ddabcfc20b3049d4b9e
karenrivasx <rivas.karen@uabc.edu.mx>
2024-04-13 01:48:49 -0700

    operaciones basicas con javascript

---
+++ practica17/practica17.js
@@ -0,0 +1,14 @@
+// datos definidos
+// var numero1 = 88;
+// var numero2 = 44;
+
+// datos ingresados por el usuario
+var numero1= parseInt(prompt("Ingrese primer numero"));
+var numero2= parseInt(prompt("Ingrese segundo numero"));
+
+var suma = numero1 + numero2;
+var resta = numero1 - numero2;
+var multiplicacion = numero1 * numero2;
+var division = numero1 / numero2;
+
+alert("La suma es: "+suma+"\nLa resta es: "+resta+" \nMultiplicacion: "+multiplicacion+" \nDivision: "+division+" \n :)");
